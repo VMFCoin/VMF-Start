2e41d773feef1f90f5967b9d412dd95a9d5f31d0
strtsk8r7 <abitofbuffalo@gmail.com>
2025-04-06 09:17:03 -0700

    Added copy to clipboard js function

--- website-build/copy-to-clipboard.js
+++ website-build/copy-to-clipboard.js
@@ -1,15 +1,13 @@
 document.querySelectorAll('.copy').forEach(function(button) {
     button.addEventListener('click', function() {
-        // Get the address span associated with the clicked button
-        var address = this.previousElementSibling.textContent.trim(); // the <span class="address"> element
-        
-        // Copy the address to the clipboard
+        var address = this.previousElementSibling.textContent.trim(); 
+
         navigator.clipboard.writeText(address)
             .then(function() {
-                alert("Copied the text: " + address); // Show success message
+                alert("Copied: " + address);
             })
             .catch(function(error) {
-                alert("Failed to copy text: " + error); // Show error if any
+                alert("Failed to copy text: " + error);
             });
     });
 });
