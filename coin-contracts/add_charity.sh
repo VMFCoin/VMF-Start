b2a92847b01f95adbfe2d5f581a59f1f64b66ee1
Kingkill666 <michael66gray@yahoo.com>
2025-06-15 19:36:56 -0700

    Deployed to base mainnet
    0x5dA3a9Ca8db3ACdE5A64077AA1D7A2408c5DE24C

--- coin-contracts/add_charity.sh
+++ coin-contracts/add_charity.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-contract_address="0x70Ff52ff9AA492390fFE09dc1521dcA76fE28811"
+contract_address="0x5dA3a9Ca8db3ACdE5A64077AA1D7A2408c5DE24C"
 charity_address="0xB697C8b4bCaE454d9dee1E83f73327D7a63600a1"
+
 source .env
-rpc_url="localhost:8485"
+rpc_url=$BASE_RPC_URL
+
+charity_address="0x6456879a5073038b0E57ea8E498Cb0240e949fC3"
+cast send $contract_address "addAllowedReceivers(address)" $charity_address \
+    --rpc-url $rpc_url --private-key "$PRIVATE_KEY"
+charity_address="0x700B53ff9a58Ee257F9A2EFda3a373D391028007"
+cast send $contract_address "addAllowedReceivers(address)" $charity_address \
+    --rpc-url $rpc_url --private-key "$PRIVATE_KEY"
+charity_address="0xB697C8b4bCaE454d9dee1E83f73327D7a63600a1"
+cast send $contract_address "addAllowedReceivers(address)" $charity_address \
+    --rpc-url $rpc_url --private-key "$PRIVATE_KEY"
+charity_address="0x5951A4160F73b8798D68e7177dF8af6a7902e725"
+cast send $contract_address "addAllowedReceivers(address)" $charity_address \
+    --rpc-url $rpc_url --private-key "$PRIVATE_KEY"
+charity_address="0xfB0EF51792c36Ae1fE6636603be199788819b67D"
+cast send $contract_address "addAllowedReceivers(address)" $charity_address \
+    --rpc-url $rpc_url --private-key "$PRIVATE_KEY"
+charity_address="0x10F01632DC709F7fA413A140739D8843b06235A1"
 cast send $contract_address "addAllowedReceivers(address)" $charity_address \
     --rpc-url $rpc_url --private-key "$PRIVATE_KEY"
--- coin-contracts/deploy.sh
+++ coin-contracts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ex
 # mainnet
-#usdc_address="0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+usdc_address="0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
 # L1 sepolia
-usdc_address="0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+#usdc_address="0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
 # base sepolia
 #usdc_address="0x036CbD53842c5426634e7929541eC2318f3dCF7e"
 
 initCharityAddress="0xf521a4fE5910b4fb4A14C9546C2837D33bEc455d"
 initTeamAddress="0xf521a4fE5910b4fb4A14C9546C2837D33bEc455d"
 source .env
-rpc_url="localhost:8485"
+rpc_url=$BASE_RPC_URL
 forge create --broadcast --rpc-url "$rpc_url" --private-key "$PRIVATE_KEY" \
     src/VmfCoin.sol:VmfCoin --constructor-args $usdc_address $initCharityAddress $initTeamAddress
